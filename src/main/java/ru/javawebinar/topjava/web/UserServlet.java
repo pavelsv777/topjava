01e230ed3c991a5fa753976d3304da6dbe32f0e1
Pavel Stulov <x_move@mail.ru>
2017-03-31 14:24:33 +0300

    HW0

---
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -0,0 +1,16 @@
+package ru.javawebinar.topjava.web;
+
+import java.io.IOException;
+
+/**
+ * Created by User on 31.03.2017.
+ */
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
